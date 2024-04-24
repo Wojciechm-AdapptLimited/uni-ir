c72bec21a9f5e7b5f356399fe3fb490f542b47ce
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-04-24 20:06:00 +0200

    refactor: add printing source and section to pretty_print_docs

--- uni_ir/utils/formatting.py
+++ uni_ir/utils/formatting.py
@@ -4,7 +4,10 @@ from langchain_core.documents import Document
 def pretty_print_docs(docs: list[Document]):
     print(
         f"\n{'-' * 100}\n".join(
-            [f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)]
+            [
+                f"\nDocument {i+1} | {d.metadata['source']} | {d.metadata['section']}:\n\n{d.page_content}"
+                for i, d in enumerate(docs)
+            ]
         )
     )
 
@@ -13,5 +16,5 @@ def format_query(query: str) -> str:
     return f"Represent this sentence for searching relevant passages: {query}"
 
 
-def format_docs(docs):
+def format_docs(docs: list[Document]):
     return "\n\n".join(doc.page_content for doc in docs)
