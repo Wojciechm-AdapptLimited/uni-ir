67ac07c0e7845b37aeec3e1c759b161bde1fedf6
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-05-09 00:52:49 +0200

    refactor(search): change the search package to use the document entity from the store package

--- uni_ir/search/index/base.py
+++ uni_ir/search/index/base.py
@@ -1,17 +1,18 @@
 from abc import ABC, abstractmethod
-from langchain_core.documents import Document
+from uuid import UUID
+
+from uni_ir.store import Document
 
 
 class BaseIndex(ABC):
-    docs: list[Document]
-
-    def __init__(self, docs: list[Document] | None = None) -> None:
-        self.docs = docs or []
-
     @abstractmethod
-    def search(self, query: str, k: int) -> list[Document]:
+    def search(self, query: str, k: int) -> list[UUID]:
         pass
 
-    @abstractmethod
     def index(self, docs: list[Document]) -> None:
+        assert all(doc.id for doc in docs), "All documents must have IDs"
+        self._index(docs)
+
+    @abstractmethod
+    def _index(self, docs: list[Document]) -> None:
         pass
