ecefd5d3c802b134fc38ded6911bf78abb8b7c4b
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-05-08 14:19:45 +0200

    feat: add a dataclass for adding examples of tool usage

--- uni_ir/formatting.py
+++ uni_ir/formatting.py
@@ -1,4 +1,7 @@
+from uuid import uuid4
 from langchain_core.documents import Document
+from langchain_core.messages import AIMessage, BaseMessage, HumanMessage, ToolMessage
+from pydantic.v1 import BaseModel
 
 
 def pretty_print_docs(docs: list[Document]):
@@ -28,3 +31,33 @@ def to_json(docs: list[Document]):
 
 def from_json(docs: list[dict]):
     return [Document(**doc) for doc in docs]
+
+
+class ToolExample(BaseModel):
+    input: str
+    calls: list[BaseModel]
+    outputs: list[str] | None = None
+
+    def to_messages(self) -> list[BaseMessage]:
+        messages: list[BaseMessage] = [HumanMessage(content=self.input)]
+        calls = []
+        outputs = self.outputs or [
+            "This is an example of a correct usage of this tool. Make sure to continue using the tool this way."
+        ] * len(self.calls)
+
+        for call in self.calls:
+            calls.append(
+                {
+                    "id": str(uuid4()),
+                    "function": {
+                        "name": call.__class__.__name__,
+                        "arguments": call.json(),
+                    },
+                }
+            )
+        messages.append(AIMessage(content="", additional_kwargs={"tool_calls": calls}))
+
+        for call, output in zip(calls, outputs):
+            messages.append(ToolMessage(tool_call_id=call["id"], content=output))
+
+        return messages
