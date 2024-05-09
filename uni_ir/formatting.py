3443d637e5af9d6dea2205ef2a98d3a5379b7987
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-05-09 11:44:15 +0200

    refactor: remove most functionality from formatting module, as it is either implemented via pydantic or directly in the main script

--- uni_ir/formatting.py
+++ uni_ir/formatting.py
@@ -1,36 +1,6 @@
 from uuid import uuid4
-from langchain_core.documents import Document
 from langchain_core.messages import AIMessage, BaseMessage, HumanMessage, ToolMessage
 from pydantic.v1 import BaseModel
-
-
-def pretty_print_docs(docs: list[Document]):
-    print(
-        f"\n{'-' * 100}\n".join(
-            [
-                f"\nDocument {i+1} | {d.metadata['source']} | {d.metadata['section']}:\n\n{d.page_content}"
-                for i, d in enumerate(docs)
-            ]
-        )
-    )
-
-
-def format_query(query: str) -> str:
-    return f"Represent this sentence for searching relevant passages: {query}"
-
-
-def format_docs(docs: list[Document]):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-
-def to_json(docs: list[Document]):
-    return [
-        {"page_content": doc.page_content, "metadata": doc.metadata} for doc in docs
-    ]
-
-
-def from_json(docs: list[dict]):
-    return [Document(**doc) for doc in docs]
 
 
 class ToolExample(BaseModel):
